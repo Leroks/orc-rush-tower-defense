1cd5b998c53525add57814f767c89b136fa44e26
Taylan Özdoğan <ozdogantaylan@gmail.com>
2023-01-06 11:29:39 +0300

    Singleton pattern created.

--- Assets/Scripts/Managers/DamageTextManager.cs
+++ Assets/Scripts/Managers/DamageTextManager.cs
@@ -2,15 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DamageTextManager : MonoBehaviour
+public class DamageTextManager : Singleton<DamageTextManager>
 {
     public ObjectPooler Pooler { get; set; }
-
-    public static DamageTextManager Instance;
-    private void Awake()
-    {
-        Instance = this;
-    }
 
     private void Start()
     {
